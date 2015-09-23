b2e2356c45a909fdfef47189810d2c248a39631e
benwang <benwanghd1981@gmail.com>
2015-09-23 19:11:03 +0800

    Add UnitTest Test_Buy_V1_x1_should_ruturn_100

---
+++ PotterShoppingCart.Tests/UnitTest_PotterShooping.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PotterShoppingCart.Tests
+{
+    [TestClass]
+    public class UnitTest_PotterShooping
+    {
+        [TestMethod]
+        public void Test_Buy_V1_x1_should_ruturn_100()
+        {
+            //arrange
+            var books = new List<Book>()
+            {
+                new Book() { Num = 1, Name = "哈利波特V1", Price = 100m},
+            };
+
+            //act
+            var target = new PotterShoppingCart();
+            decimal expected = 100m;
+            decimal actual =  PotterShoppingCart.Calculate(books);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
