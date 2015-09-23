5e868b384ff883e96b441fc795e9558f84a5e3c7
benwang <benwanghd1981@gmail.com>
2015-09-23 19:12:53 +0800

    Add Book model

---
+++ Day2HWLib/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Day2HWLib
+{
+    public class Book
+    {
+        public int Num { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
--- PotterShoppingCart.Tests/UnitTest_PotterShooping.cs
+++ PotterShoppingCart.Tests/UnitTest_PotterShooping.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Day2HWLib;
 
 namespace PotterShoppingCart.Tests
 {
