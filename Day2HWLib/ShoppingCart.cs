fb75bc26e6e6ac7e368a81e7e915a1390147d675
benwang <benwanghd1981@gmail.com>
2015-09-23 19:25:27 +0800

    Add ShoppingCart

---
+++ Day2HWLib/ShoppingCart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Day2HWLib
+{
+    public class ShoppingCart
+    {
+        public decimal Calculate(List<Book> books)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
