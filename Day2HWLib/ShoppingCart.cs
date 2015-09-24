22c6b2f3417344cad2a456fb7dc12ce005ac9256
benwang <benwanghd1981@gmail.com>
2015-09-24 11:13:07 +0800

    Modify Calculate for Test_Buy_V1_And_V2_And_V3_And_V4_And_V5_x1_should_ruturn_375

--- Day2HWLib/ShoppingCart.cs
+++ Day2HWLib/ShoppingCart.cs
@@ -35,6 +35,9 @@ namespace Day2HWLib
                 case 4:
                     discount = 0.8m;
                     break;
+                case 5:
+                    discount = 0.75m;
+                    break;
             }
 
             return discount;
