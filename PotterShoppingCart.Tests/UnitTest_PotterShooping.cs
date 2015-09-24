0788682df4492d3c1d8e29b9a62c17180bf25b1b
benwang <benwanghd1981@gmail.com>
2015-09-24 10:44:31 +0800

    Add Test_Buy_V1_And_V2_x1_should_ruturn_190

--- PotterShoppingCart.Tests/UnitTest_PotterShooping.cs
+++ PotterShoppingCart.Tests/UnitTest_PotterShooping.cs
@@ -24,5 +24,23 @@ namespace PotterShoppingCart.Tests
             //assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void Test_Buy_V1_And_V2_x1_should_ruturn_190()
+        {
+            //arrange
+            var books = new List<Book>()
+            {
+                new Book() { Num = 1, Name = "哈利波特V1", Price = 100m},
+                new Book() { Num = 2, Name = "哈利波特V2", Price = 100m},
+            };
+
+            //act
+            var target = new ShoppingCart();
+            decimal expected = 190;
+            decimal actual = target.Calculate(books);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
